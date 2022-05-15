e8107d29bddbc5fcaebf68214f6b5cc095ea1f40
chen-semu <chensemu@gmail.com>
2022-05-15 20:20:16 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+/*function maxNum(){
+    var num1=+prompt();
+    var num2=+prompt();
+    var result;
+    if(num1>num2){result=num1}
+    else{result=num2}
+    return result;
+}
+
+document.write(maxNum())
+function sumNum() {
+  var sum=0;
+  for (var i =+prompt(); i > 0; i--) {
+    sum += i;
+  }
+  return sum;
+}
+document.write(sumNum());
+function replace(num1,num2){
+   var helper=num1;
+   num1=num2;
+   num2=helper;
+}
+var numFirst;
+var numSecond;
+document.write()*/
+// function power(){
+// var num =+prompt()
+// var help=num
+// var Ipower=+prompt()
+// for(i=1;i<Ipower;i++){
+//   num*=help
+// }
+// return num
+// }
+// document.write(power())
+// function fibo(){
+//   var num1=1;
+//   var num2=0;
+//   for(i=+prompt();i>0;i--){
+//     var fib=num1
+//     num1+=num2
+//     num2=fib
+//   }
+//   return num2
+// }
+// document.write(fibo())
+// function stars(){
+  
+// }
+// var maarach=[ "or", "dalia","abaynech","yonatan","znavo"]
+// console.log(maarach)
+// console.log(maarach[2])
+//   for(i=0;i<4;i++){
+//     console.log(maarach[i])
+// }
+// var sidra=[129,186,285,477,324,565,754]
+// console.log(sidra)
+// console.log(sidra[3])
+// for(i=0;i<7;i++){
+//   console.log(sidra[i])
+// }
+
+// var sidra2=[22,45,35,33,34,28,49,32,44,24]
+//   console.log(sidra2)
+//   for(i=0;i<10;i++){
+//     if(sidra2[i]%2==0){
+//       console.log(sidra2[i])
+//     }
+//   }
+//   var myarray=["hello",2,"world","jacob",6,7,"bye",98]
+//   for(i=0;i<8;i++){
+//     if(myarray[i]*0!=0){
+//       console.log(myarray[i])
+//     }
+//   }
+//  var startedEmpty=[]
+//  startedEmpty.push(9)
+//  for(i=0;i<=9;i++){
+//    startedEmpty.push(i)
+//  }
+//  console.log(startedEmpty)
+ 
